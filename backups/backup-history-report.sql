2608c9864d3ca553046f29fa8ac0e0ec998c5aeb
Martyn Figueiredo <martyn@exacrafter.com>
2025-08-04 21:46:40 -0300

    Update backup-history-report.sql

--- backups/backup-history-report.sql
+++ backups/backup-history-report.sql
@@ -1,16 +1,16 @@
 /*
     backup-history-report.sql
-    Returns a chronological list of backup events with useful details.
+    Returns a detailed chronological list of backup events.
 
-    Shows database name, backup start and finish times, duration, backup file, backup type, backup size and compressed size.
 */
 
 SELECT
-    bs.database_name           AS DatabaseName,
-    bs.backup_start_date       AS BackupStart,
-    bs.backup_finish_date      AS BackupFinish,
-    DATEDIFF(MINUTE, bs.backup_start_date, bs.backup_finish_date) AS DurationMinutes,
-    bmf.physical_device_name   AS BackupFile,
+    bs.database_name         AS DatabaseName,
+    bs.server_name           AS ServerName,
+    bs.backup_start_date     AS BackupStart,
+    bs.backup_finish_date    AS BackupFinish,
+    DATEDIFF(MILLISECOND, bs.backup_start_date, bs.backup_finish_date) AS DurationMilliseconds,
+    bmf.physical_device_name AS BackupFile,
     CASE bs.[type]
          WHEN 'D' THEN 'Full Backup'
          WHEN 'I' THEN 'Differential Backup'
@@ -19,12 +19,14 @@ SELECT
          WHEN 'G' THEN 'Differential File'
          WHEN 'P' THEN 'Partial'
          WHEN 'Q' THEN 'Differential Partial'
-    END AS BackupType,
-    ROUND(bs.backup_size / 1024.0 / 1024.0, 2)           AS BackupSizeMB,
-    ROUND(bs.compressed_backup_size / 1024.0 / 1024.0, 2) AS CompressedSizeMB
+    END                       AS BackupType,
+    CAST(ROUND(bs.backup_size / 1024.0 / 1024.0, 2) AS DECIMAL(18,2))           AS BackupSizeMB,
+    CAST(ROUND(bs.compressed_backup_size / 1024.0 / 1024.0, 2) AS DECIMAL(18,2)) AS CompressedSizeMB,
+    bs.user_name             AS BackupUser,
+    bs.is_copy_only          AS IsCopyOnly
 FROM msdb.dbo.backupset bs
 JOIN msdb.dbo.backupmediafamily bmf
     ON bs.media_set_id = bmf.media_set_id
--- Optional: filter by time period
--- WHERE bs.backup_finish_date >= DATEADD(day, -30, GETDATE())
+-- Optionally filter to recent history, e.g. last 30 days:
+--WHERE bs.backup_finish_date >= DATEADD(day, -30, GETDATE())
 ORDER BY bs.backup_start_date DESC;

4fdfa4a3530dd5a55c55b9f15476c126561be7f3
Martyn Figueiredo <martyn@exacrafter.com>
2025-08-06 17:37:49 -0300

    Update database-mail-profile-security.sql

--- high-compliance/database-mail-profile-security.sql
+++ high-compliance/database-mail-profile-security.sql
@@ -8,15 +8,16 @@ SELECT
     p.profile_id,
     p.name AS ProfileName,
     p.description,
-    sp.principal_id,
+    pp.is_default AS IsDefault,
+    CONVERT(VARCHAR(85), pp.principal_sid, 1) AS PrincipalSID,
     sp.name AS PrincipalName,
     sp.type_desc AS PrincipalType,
     sp.is_disabled,
     sp.create_date,
-    sp.default_database_name,
-    pp.is_default AS IsDefault
-FROM msdb.dbo.sysmail_principalprofile AS pp
-JOIN msdb.dbo.sysmail_profile AS p
+    sp.default_database_name
+FROM msdb.dbo.sysmail_profile AS p
+JOIN msdb.dbo.sysmail_principalprofile AS pp
     ON pp.profile_id = p.profile_id
 LEFT JOIN sys.server_principals AS sp
-    ON pp.principal_id = sp.principal_id;
+    ON pp.principal_sid = sp.sid
+ORDER BY p.name, sp.name;
